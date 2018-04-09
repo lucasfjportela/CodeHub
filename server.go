b7e8ccde549739855733b217db0c3340d8d29f7c
Matheus-felipe <matheus0906.mhci@gmail.com>
2018-04-09 15:53:10 -0300

    First server

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+/*Func that execute the server operation*/
+func handleServerConnection(conn net.Conn) {
+	readed := make([]byte, 1024)
+	datasize, _ := conn.Read(readed)
+	data := readed[:datasize]
+	fmt.Print(string(data))
+}
+
+func main() {
+
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "localhost:2222")
+	test, _ := net.ListenTCP("tcp", tcpAddr)
+
+	var tcpConn net.TCPConn
+
+	defer tcpConn.Close()
+
+	for {
+		fmt.Println("Rola antes")
+		tcpConn, _ := test.Accept()
+		fmt.Println("Rola depois")
+		go handleServerConnection(tcpConn)
+	}
+
+}
