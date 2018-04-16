6813a361a6d141e6a5f94b996a00322cd78542ab
Matheus-felipe <matheus0906.mhci@gmail.com>
2018-04-15 22:41:54 -0300

    Add Ftp client-server communication

--- client.go
+++ client.go
@@ -3,8 +3,10 @@ package main
 import (
 	"codehub-sd/messageFormat"
 	"encoding/gob"
-	"fmt"
 	"net"
+	"os"
+
+	"github.com/jlaffaye/ftp"
 )
 
 /*
@@ -61,9 +63,29 @@ func handleClientServerConnection(conn *net.TCPConn, msgUser messageFormat.Messa
 
 func main() {
 
+	conn, err := ftp.Dial("localhost:2121")
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Login("admin", "admin")
+
+	if err != nil {
+		panic(err)
+	}
+
+	file, fError := os.Open("TETEU.txt")
+
+	if fError != nil {
+		panic(fError)
+	}
+
+	conn.Stor("/User/Theu.txt", file)
+
 	//var list ListUsers
 	//pl := {"bean", "123"}.([]string)
-	tcpAddrAUTH, _ := net.ResolveTCPAddr("tcp", "localhost:1115")
+
+	/*tcpAddrAUTH, _ := net.ResolveTCPAddr("tcp", "localhost:1115")
 	tcpAddrDNS, _ := net.ResolveTCPAddr("tcp", "localhost:2223")
 
 	conn, _ := net.DialTCP("tcp", nil, tcpAddrAUTH)
@@ -72,22 +94,25 @@ func main() {
 
 	ver := handleClientAuthConnection(conn, msgUser)
 
-	/*Verify User auth*/
 	if ver == false {
 		fmt.Println("Lixo, não pode conectar")
 	} else {
-
+		/*
 		conn2, _ := net.DialTCP("tcp", nil, tcpAddrDNS)
 
 		serverAddr := handleClientDNSConnection(conn2)
 
 		fmt.Println(serverAddr)
+	*/
+	//handleClientServerConnection(conn, msgUser)
 
+	/*
 		tcpAddrServer, _ := net.ResolveTCPAddr("tcp", serverAddr)
 		conn, _ := net.DialTCP("tcp", nil, tcpAddrServer)
 		msgUser = messageFormat.MessageFormat{Origin: "Client", ReqType: "sto"}
 		handleClientServerConnection(conn, msgUser)
-	}
+	*/
+	//}
 
 	/*encoder := gob.NewEncoder(conn)
 	err := encoder.Encode(a)
--- server.go
+++ server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"encoding/gob"
-	"fmt"
 	"net"
 
-	"../codehub-sd/messageFormat"
+	filedriver "github.com/goftp/file-driver"
+	"github.com/goftp/server"
 )
+
+type TestAuth struct {
+	Name     string
+	Password string
+}
+
+func (a *TestAuth) CheckPasswd(name, pass string) (bool, error) {
+	if name != a.Name || pass != a.Password {
+		return false, nil
+	}
+	return true, nil
+}
 
 /*Func that execute the server operation*/
 func handleServerConnection(conn *net.TCPConn) {
@@ -22,25 +33,26 @@ func handleServerConnection(conn *net.TCPConn) {
 	//data := readed[:datasize]
 	fmt.Println(data)
 	*/
-	msg := messageFormat.MessageFormat{}
+	/*
+		msg := messageFormat.MessageFormat{}
 
-	decoder := gob.NewDecoder(conn)
-	decoder.Decode(&msg)
+		decoder := gob.NewDecoder(conn)
+		decoder.Decode(&msg)
 
-	if msg.Origin == "Client" {
-		if msg.ReqType == "sto" {
-			fmt.Println("Aqui guarda o arquivo")
-		}
+		if msg.Origin == "Client" {
+			if msg.ReqType == "sto" {
+				fmt.Println("Aqui guarda o arquivo")
+			}
 
-		if msg.ReqType == "get" {
-			fmt.Println("Aqui dá o arquivo pra ele")
-		}
-	}
+			if msg.ReqType == "get" {
+				fmt.Println("Aqui dá o arquivo pra ele")
+			}
+		}*/
 
 }
 
 func main() {
-	fmt.Println("Starting Server...")
+	/*fmt.Println("Starting Server...")
 	tcpAddr, _ := net.ResolveTCPAddr("tcp", "localhost:1111")
 	listener, _ := net.ListenTCP("tcp", tcpAddr)
 
@@ -54,5 +66,24 @@ func main() {
 		fmt.Println("Dále")
 		go handleServerConnection(tcpConn)
 	}
+	*/
+	factory := &filedriver.FileDriverFactory{
+		RootPath: "C:/Users/Matheus/Desktop/ROLA",
+		Perm:     server.NewSimplePerm("root", "root"),
+	}
+
+	opts := &server.ServerOpts{
+		Factory:  factory,
+		Port:     2121,
+		Hostname: "localhost",
+		Auth: &TestAuth{
+			Name:     "admin",
+			Password: "admin",
+		},
+	}
+
+	serv := server.NewServer(opts)
+
+	serv.ListenAndServe()
 
 }
